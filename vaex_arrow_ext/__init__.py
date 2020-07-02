fa5df6034faf431b7b507be337b7c9ea9c415064
Maarten A. Breddels <maartenbreddels@gmail.com>
2020-07-02 20:50:53 +0200

    try osx and windows

--- vaex_arrow_ext/__init__.py
+++ vaex_arrow_ext/__init__.py
@@ -1,14 +1,16 @@
 from pathlib import Path
-import ctypes
 import pyarrow as pa
+import platform
 
-def _set_arrow_symbol_resolution(flag):
-    for dir in map(Path, pa.get_library_dirs()):
-        arrow_path = dir / 'libarrow.so'
-        arrow_python_path = dir / 'libarrow_python.so'
-        if arrow_path.exists() and arrow_python_path.exists():
-            arrow_python = ctypes.CDLL(arrow_path, flag)
-            libarrow_python = ctypes.CDLL(arrow_python_path, flag)
-            break
-_set_arrow_symbol_resolution(ctypes.RTLD_GLOBAL)
+if platform.system().lower() == 'linux':
+    import ctypes
+    def _set_arrow_symbol_resolution(flag):
+        for dir in map(Path, pa.get_library_dirs()):
+            arrow_path = dir / 'libarrow.so'
+            arrow_python_path = dir / 'libarrow_python.so'
+            if arrow_path.exists() and arrow_python_path.exists():
+                arrow_python = ctypes.CDLL(arrow_path, flag)
+                libarrow_python = ctypes.CDLL(arrow_python_path, flag)
+                break
+    _set_arrow_symbol_resolution(ctypes.RTLD_GLOBAL)
 from . import ext
