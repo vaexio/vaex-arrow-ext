a126d32952a1daf3cf8f1e6469bb7f4a2cb475e4
Maarten A. Breddels <maartenbreddels@gmail.com>
2020-07-02 18:49:57 +0200

    make symbols of arrow and arrow_python global

---
+++ vaex_arrow_ext/__init__.py
@@ -0,0 +1,12 @@
+from pathlib import Path
+import ctypes
+import pyarrow as pa
+
+
+for dir in map(Path, pa.get_library_dirs()):
+    arrow_path = dir / 'libarrow.so'
+    arrow_python_path = dir / 'libarrow_python.so'
+    if arrow_path.exists() and arrow_python_path.exists():
+        arrow_python = ctypes.CDLL(arrow_path, ctypes.RTLD_GLOBAL)
+        libarrow_python = ctypes.CDLL(arrow_python_path, ctypes.RTLD_GLOBAL)
+        break
